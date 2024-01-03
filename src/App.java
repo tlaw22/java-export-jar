aee76fc0c1dce84fc3aba82bbe24660a99ae32f9
tim <timothy.lawless@gmail.com>
2024-01-03 18:30:09 -0500

    finished

--- src/App.java
+++ src/App.java
@@ -1,4 +1,9 @@
 public class App {
+// To Export a Jar executable in VSCODE
+// Open the command palette CTRL + SHIFT + P
+// Type Export
+// Select Java: Export Jar
+
     public static void main(String[] args) throws Exception {
         System.out.println("The program run successfully");
     }
