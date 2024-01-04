bac5f8d0ad3af765146784bac646daa03917f970
tim <timothy.lawless@gmail.com>
2024-01-03 19:21:10 -0500

    none

--- src/App.java
+++ src/App.java
@@ -11,7 +11,7 @@ public class App {
 
 // To Export a jar from the terminal
 // Create a mainfest.mf file and specify the main class. Main-Class: App
-// The manifest must have 1 blank link below the main class decloration
+// The manifest must have 1 blank line below the main class decloration
 // Type jar -cmf manifest.mf myjarfile.jar *.class
 // into the terminal
 // type java -jar myjarfile.jar to run the program
